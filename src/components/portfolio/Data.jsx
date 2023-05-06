179fbbd61d09b13e62792611f2479632a784f8d3
Edis Doric <edisdoricvevo@gmail.com>
2023-05-06 12:34:03 +0200

    portfolio update

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -10,91 +10,211 @@ import Work9 from "../../assets/newsletter.png";
 import Work10 from "../../assets/dicegame.png";
 import Work11 from "../../assets/form.jpg";
 import Work12 from "../../assets/todolist.png";
+import Work13 from "../../assets/candycrushgame.jpg";
+import Work14 from "../../assets/tictactoegame.png";
+import Work15 from "../../assets/2048game.png";
+import Work16 from "../../assets/snakegame.png";
+import Work17 from "../../assets/ponggame.png";
+import Work18 from "../../assets/tetrisgame.png";
+import Work19 from "../../assets/dinogame.png";
+import Work20 from "../../assets/alexandrapr.jpg";
+import Work21 from "../../assets/centraldowntown.jpg";
+import Work22 from "../../assets/boostr.jpg";
+import Work23 from "../../assets/fidabio.jpg";
+import Work24 from "../../assets/syncfab.jpg";
+import Work25 from "../../assets/smartmfg.png";
+import Work26 from "../../assets/therealbonnevivante.jpg";
+import Work27 from "../../assets/brightsy.png";
 
 export const projectsData = [
   {
-    id: 1,
+    id: 8,
     image: Work1,
     title: "Responsive Blog 1",
     category: "static sites",
     url: "https://responsive-website-design-1.onrender.com",
   },
   {
-    id: 2,
+    id: 9,
     image: Work2,
     title: "Responsive Blog 2",
     category: "static sites",
     url: "https://responsive-website-design-2.onrender.com",
   },
   {
-    id: 3,
+    id: 23,
     image: Work3,
     title: "Simon Game",
     category: "static sites",
     url: "https://simon-game-zgjq.onrender.com",
   },
   {
-    id: 4,
+    id: 26,
     image: Work4,
     title: "Drum Kit Game",
     category: "static sites",
     url: "https://drum-kit-game.onrender.com",
   },
   {
-    id: 5,
+    id: 10,
     image: Work5,
     title: "Live Site - Competex",
     category: "live sites",
     url: "https://competex.de/",
   },
   {
-    id: 6,
+    id: 11,
     image: Work6,
     title: "Live Site - Gle Berlin",
     category: "live sites",
     url: "https://gle-berlin.de/",
   },
   {
-    id: 7,
+    id: 4,
     image: Work7,
     title: "Live Site - Arcadia",
     category: "live sites",
     url: "https://arcadianewengland.com/",
   },
   {
-    id: 8,
+    id: 24,
     image: Work8,
     title: "Live Site - InstantAdwiser",
     category: "live sites",
     url: "https://instantadwiser.com/",
   },
   {
-    id: 9,
+    id: 16,
     image: Work9,
     title: "Newsletter Signup Page",
     category: "dynamic sites",
     url: "https://newsletter-signup-page-4q1q.onrender.com",
   },
   {
-    id: 10,
+    id: 25,
     image: Work10,
     title: "Dice Game",
     category: "static sites",
     url: "https://dice-game-xmu0.onrender.com",
   },
   {
-    id: 11,
+    id: 27,
     image: Work11,
     title: "Simple Survey Form",
     category: "static sites",
     url: "https://form-survey-form.onrender.com",
   },
   {
-    id: 12,
+    id: 17,
     image: Work12,
     title: "To Do List",
     category: "dynamic sites",
     url: "https://to-do-list-database.onrender.com/",
+  },
+  {
+    id: 13,
+    image: Work13,
+    title: "Candy Crush",
+    category: "static sites",
+    url: "https://javascript-candy-crush.netlify.app/",
+  },
+  {
+    id: 6,
+    image: Work14,
+    title: "Tic Tac Toe",
+    category: "static sites",
+    url: "https://edis-tic-tac-toe.netlify.app/",
+  },
+  {
+    id: 5,
+    image: Work15,
+    title: "2048",
+    category: "static sites",
+    url: "https://javascript-2048-edis.netlify.app/",
+  },
+  {
+    id: 12,
+    image: Work16,
+    title: "Snake",
+    category: "static sites",
+    url: "https://javascript-snake-edis.netlify.app/",
+  },
+  {
+    id: 14,
+    image: Work17,
+    title: "Pong",
+    category: "static sites",
+    url: "https://javascript-pong-edis.netlify.app/",
+  },
+  {
+    id: 7,
+    image: Work18,
+    title: "Tetris",
+    category: "static sites",
+    url: "https://javascript-tetris-edis.netlify.app/",
+  },
+  {
+    id: 15,
+    image: Work19,
+    title: "Dino",
+    category: "static sites",
+    url: "https://javascript-dino-game.netlify.app/",
+  },
+  {
+    id: 2,
+    image: Work24,
+    title: "Live Site - SyncFab",
+    category: "live sites",
+    url: "https://syncfab.com/",
+  },
+  {
+    id: 1,
+    image: Work25,
+    title: "Live Site - SmartMFG",
+    category: "live sites",
+    url: "https://smartmfg.io/",
+  },
+  {
+    id: 22,
+    image: Work20,
+    title: "Live Site - AlexandraPR",
+    category: "live sites",
+    url: "https://alexandrapr.com/en/",
+  },
+  {
+    id: 21,
+    image: Work21,
+    title: "Live Site - Central Downtown",
+    category: "live sites",
+    url: "https://central-downtown.com/",
+  },
+  {
+    id: 3,
+    image: Work22,
+    title: "Live Site - Boostr",
+    category: "live sites",
+    url: "https://www.boostr.com/",
+  },
+  {
+    id: 18,
+    image: Work23,
+    title: "Live Site - FidaBio",
+    category: "live sites",
+    url: "https://fidabio.com/",
+  },
+  {
+    id: 19,
+    image: Work26,
+    title: "Live Site - The Real Bonne Vivante",
+    category: "live sites",
+    url: "https://therealbonnevivante.com/",
+  },
+  {
+    id: 20,
+    image: Work27,
+    title: "Live Site - Brightsy",
+    category: "live sites",
+    url: "http://unbouncepages.com/brightsy",
   },
 ];
 
